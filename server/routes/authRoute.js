c186915829095c47f4870137b1bfaf64fcc3eddc
Andy Garcia <acgarzeah@gmail.com>
2021-01-16 13:14:35 -0800

    updated status codes

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -4,7 +4,7 @@ const router = new express.Router();
 const User = require("../models/User");
 
 // User signup route
-router.post("/signup", async (req, res) => {
+router.post("/register", async (req, res) => {
 	const user = new User(req.body);
 
 	try {
@@ -22,7 +22,7 @@ router.post("/login", async (req, res) => {
 			req.body.email,
 			req.body.password
 		);
-		res.send(user);
+		res.status(201).send(user);
 	} catch (err) {
 		res.status(400).send(err);
 	}
