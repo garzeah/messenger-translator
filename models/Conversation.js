7cd33bc5b37020134b68e85cde4ebf209cb588b9
Andy Garcia <acgarzeah@gmail.com>
2021-01-26 18:54:12 -0800

    fixed merge issues

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+// Our schema to keep track of conversation between users
 const conversationSchema = new mongoose.Schema({
 	participants: [
 		{
+			// This will contain each participant's mongo objectID
 			participant: {
 				type: mongoose.Schema.Types.ObjectId,
 				ref: "User"
--- routes/conversationRoute.js
+++ routes/conversationRoute.js
@@ -6,25 +6,25 @@ const Conversation = require("../models/Conversation");
 const verifyToken = require("../middlewares/verifyToken");
 
 // Create a conversation
-router.post("/api/conversations/new/", verifyToken, async (req, res) => {
+router.post("/conversations/new/", verifyToken, async (req, res) => {
 	try {
-		// Retrieving the ._ids of the person initiating and receiving the conversation
-		const sender = await User.findById({ _id: req.user._id });
 		const receiver = await User.findOne({ email: req.body.email });
 
 		// Checking to see if conversation already exists
 		const conversation = await Conversation.findOne({
-			"participants.participant": sender._id,
+			// Saving Sender and Reciver's ID
+			"participants.participant": req.user._id,
 			"participants.participant": receiver._id
 		});
 
+		// If a conversation already exists, return that conversation
 		if (conversation) return res.send(conversation);
 
 		// Otherwise, lets create a new conversation and save it
 		const newConversation = new Conversation({
 			participants: [
 				{
-					participant: sender._id
+					participant: req.user._id
 				},
 				{
 					participant: receiver._id
@@ -40,7 +40,7 @@ router.post("/api/conversations/new/", verifyToken, async (req, res) => {
 });
 
 // Get all conversations for a user
-router.get("/api/conversations/", verifyToken, async (req, res) => {
+router.get("/conversations/", verifyToken, async (req, res) => {
 	// Finds all conversations the user is associated with
 	const conversations = await Conversation.find({
 		"participants.participant": req.user._id
@@ -49,9 +49,9 @@ router.get("/api/conversations/", verifyToken, async (req, res) => {
 });
 
 // Get a specific conversation
-router.get("/api/conversations/:id", (req, res) => {});
+router.get("/conversations/:id", (req, res) => {});
 
 // Send message
-router.post("/api/conversations/:id", (req, res) => {});
+router.post("/conversations/:id", (req, res) => {});
 
 module.exports = router;
